28263609ed6661eeae274037361cd1f00527e32b
Filip <filip7243@gmail.com>
2023-03-20 22:39:01 +0100

    refactored ToDoItemRepo

--- src/main/java/com/productivity_helper/app/repo/ToDoItemRepository.java
+++ src/main/java/com/productivity_helper/app/repo/ToDoItemRepository.java
@@ -1,15 +1,13 @@
 package com.productivity_helper.app.repo;
 
 import com.productivity_helper.app.model.ToDoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ToDoItemRepository {
+public interface ToDoItemRepository extends JpaRepository<ToDoItem, Long> {
 
-    void addToDoItem(ToDoItem toDoItem);
-    boolean removeToDoItem(Long id);
-    boolean modifyToDoItem(Long oldToDoItemId, ToDoItem newToDoItem);
-    List<ToDoItem> findUserToDoItems(Long userId);
+    List<ToDoItem> findToDoItemByUserId(Long userId);
     void markToDoItemAsComplete(ToDoItem item);
     void markToDoItemAsUnDone(ToDoItem item);
 
