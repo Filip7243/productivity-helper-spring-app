c1b6a4aad2840cde4d81ce3d6f5e3762e12e203b
Filip <filip7243@gmail.com>
2023-03-20 22:08:26 +0100

    added methods to repo

--- src/main/java/com/productivity_helper/app/repo/ToDoItemRepository.java
+++ src/main/java/com/productivity_helper/app/repo/ToDoItemRepository.java
@@ -1,4 +1,16 @@
 package com.productivity_helper.app.repo;
 
+import com.productivity_helper.app.model.ToDoItem;
+
+import java.util.List;
+
 public interface ToDoItemRepository {
+
+    void addToDoItem(ToDoItem toDoItem);
+    boolean removeToDoItem(Long id);
+    boolean modifyToDoItem(Long oldToDoItemId, ToDoItem newToDoItem);
+    List<ToDoItem> findUserToDoItems(Long userId);
+    void markToDoItemAsComplete(ToDoItem item);
+    void markToDoItemAsUnDone(ToDoItem item);
+
 }
