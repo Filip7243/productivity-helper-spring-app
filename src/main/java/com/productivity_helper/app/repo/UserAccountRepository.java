bd0c358b4b92d8447f68b97651e23f965c59dd17
Filip <filip7243@gmail.com>
2023-03-21 15:58:45 +0100

    added @Repository adnottaion

--- src/main/java/com/productivity_helper/app/repo/UserAccountRepository.java
+++ src/main/java/com/productivity_helper/app/repo/UserAccountRepository.java
@@ -2,6 +2,8 @@ package com.productivity_helper.app.repo;
 
 import com.productivity_helper.app.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserAccountInterface extends JpaRepository<UserAccount, Long> {
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 }
