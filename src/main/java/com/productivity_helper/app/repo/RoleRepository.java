ba53a65568d7583674bd7fdf9963edc7ab77da87
Filip <filip7243@gmail.com>
2023-03-21 16:17:48 +0100

    added @Modifying and @Query to update method

--- src/main/java/com/productivity_helper/app/repo/RoleRepository.java
+++ src/main/java/com/productivity_helper/app/repo/RoleRepository.java
@@ -2,6 +2,9 @@ package com.productivity_helper.app.repo;
 
 import com.productivity_helper.app.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,7 +18,9 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Boolean removeRoleByName(String name);
 
-    void updateRoleWithId(Long id, Role updatedRole);
+    @Modifying
+    @Query("UPDATE Role r SET r.name = :name WHERE r.id = :id")
+    void updateRoleWithId(@Param("id") Long id, @Param("name") String name);
 
     Boolean existsByName(String name);
 }
