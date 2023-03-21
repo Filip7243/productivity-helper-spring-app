2d2fa927f0b8dae0b73a30a18c278cb135c47148
Filip <filip7243@gmail.com>
2023-03-21 17:31:49 +0100

    added update method tests

--- src/test/java/com/productivity_helper/app/repo/RoleRepositoryTest.java
+++ src/test/java/com/productivity_helper/app/repo/RoleRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.productivity_helper.app.repo;
 
 import com.productivity_helper.app.model.Role;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
@@ -46,7 +49,7 @@ public class RoleRepositoryTest {
     }
 
     @Test
-    public void whenSaved_thenFindsByName() {
+    public void itShouldFindRoleByNameAfterSave() {
         Role newRole = new Role(null, "NEW_ROLE", new HashSet<>());
 
         roleRepository.save(newRole);
@@ -55,16 +58,45 @@ public class RoleRepositoryTest {
         makeSureThatRoleWasFound(foundRole);
     }
 
+    @Test
+    public void itShouldNotSaveRoleBecauseRoleAlreadyExists() {
+        Role newRole = new Role(null, "ROLE_ADMIN", new HashSet<>()); // role already exists
+
+        assertThrows(DataIntegrityViolationException.class, () -> roleRepository.save(newRole));
+        makeSureThatRoleWasFound(Optional.of(newRole));
+    }
+
+    @Test
+    public void itShouldUpdateRoleWithId() {
+        Optional<Role> foundRole = roleRepository.findById(1L);
+        String oldName = foundRole.get().getName();
+        String newName = "ROLE_TEST";
+
+        int updatedRows = roleRepository.updateRoleWithId(foundRole.get().getId(), newName);
+        Optional<Role> oldRole = roleRepository.findByName(oldName);
+        Optional<Role> newRole = roleRepository.findByName(newName);
+
+        assertEquals(updatedRows, 1); // 1 row was updated
+        makeSureThatRoleWasNotFound(oldRole);
+        makeSureThatRoleWasFound(newRole);
+    }
+
+    @Test
+    public void itShouldNotUpdateRoleWithIdBecauseDoesntExist() {
+        String newName = "ROLE_TEST";
+
+        int updatedRows = roleRepository.updateRoleWithId(0L, newName);
+
+        assertEquals(updatedRows, 0); // 1 row was updated
+    }
 
     private static void makeSureThatRoleWasFound(Optional<Role> foundRole) {
-        assertThat(foundRole).isInstanceOf(Optional.class);
         assertThat(foundRole).isPresent();
         assertThat(foundRole).isNotNull();
         assertThat(foundRole.get()).isInstanceOf(Role.class);
     }
 
     private static void makeSureThatRoleWasNotFound(Optional<Role> foundRole) {
-        assertThat(foundRole).isInstanceOf(Optional.class);
         assertThat(foundRole).isNotPresent();
         assertThat(foundRole).isNotNull();
     }
