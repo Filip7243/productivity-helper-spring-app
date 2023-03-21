38a8c4032a970da42f38360b1663a60a87a58841
Filip <filip7243@gmail.com>
2023-03-21 15:56:29 +0100

    updated RoleReposiotry

--- src/main/java/com/productivity_helper/app/repo/RoleRepository.java
+++ src/main/java/com/productivity_helper/app/repo/RoleRepository.java
@@ -1,2 +1,21 @@
-package com.productivity_helper.app.repo;public interface RoleRepository {
+package com.productivity_helper.app.repo;
+
+import com.productivity_helper.app.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+    Boolean removeRoleById(Long id);
+
+    Boolean removeRoleByName(String name);
+
+    void updateRoleWithId(Long id, Role updatedRole);
+
+    Boolean existsByName(String name);
 }
