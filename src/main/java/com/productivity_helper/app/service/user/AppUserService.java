f7df8b80ac2ef6fa597735b18cd7550bbcd60521
Filip <filip7243@gmail.com>
2023-04-13 13:48:46 +0200

    load user impl

--- src/main/java/com/productivity_helper/app/service/user/AppUserService.java
+++ src/main/java/com/productivity_helper/app/service/user/AppUserService.java
@@ -1,13 +1,21 @@
 package com.productivity_helper.app.service.user;
 
+import com.productivity_helper.app.repo.user.AppUserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null; // findByUserName()
+        return userRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
     }
 }
