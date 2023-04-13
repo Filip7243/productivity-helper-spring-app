8860c674e25d13536bb8de467c52d14f0588b6df
Filip <filip7243@gmail.com>
2023-04-13 13:19:09 +0200

    added findByName()

--- src/main/java/com/productivity_helper/app/repo/user/AppUserRoleRepository.java
+++ src/main/java/com/productivity_helper/app/repo/user/AppUserRoleRepository.java
@@ -1,2 +1,13 @@
-package com.productivity_helper.app.repo.user;public class AppUserRoleRepository {
+package com.productivity_helper.app.repo.user;
+
+import com.productivity_helper.app.model.user.AppUserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRoleRepository extends JpaRepository<AppUserRole, Long> {
+
+    Optional<AppUserRole> findByName(String name);
 }
