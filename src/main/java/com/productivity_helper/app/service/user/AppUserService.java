83d4a6d31b767bb6c7b34755bdb74488c7882c1e
Filip <filip7243@gmail.com>
2023-04-13 12:33:33 +0200

    implement UserDetailsService

--- src/main/java/com/productivity_helper/app/service/user/AppUserService.java
+++ src/main/java/com/productivity_helper/app/service/user/AppUserService.java
@@ -1,2 +1,13 @@
-package com.productivity_helper.app.service.user;public class AppUserService {
+package com.productivity_helper.app.service.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return null; // findByUserName()
+    }
 }
