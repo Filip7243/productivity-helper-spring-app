2c6554cc17a99052195bfc5e577b256b20fe22ff
Filip <filip7243@gmail.com>
2023-04-13 13:18:37 +0200

    added findByEmail() method

--- src/main/java/com/productivity_helper/app/repo/user/AppUserRepository.java
+++ src/main/java/com/productivity_helper/app/repo/user/AppUserRepository.java
@@ -1,2 +1,13 @@
-package com.productivity_helper.app.repo.user;public class AppUserRepository {
+package com.productivity_helper.app.repo.user;
+
+import com.productivity_helper.app.model.user.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByEmail(String email);
 }
