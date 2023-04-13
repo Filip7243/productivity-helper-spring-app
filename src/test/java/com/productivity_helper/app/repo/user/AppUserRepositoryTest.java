2a4a3bc303f370b18ec23ae4a24f9aee72b9374d
Filip <filip7243@gmail.com>
2023-04-13 13:20:05 +0200

    added basic tests for find methods

--- src/test/java/com/productivity_helper/app/repo/user/AppUserRepositoryTest.java
+++ src/test/java/com/productivity_helper/app/repo/user/AppUserRepositoryTest.java
@@ -1,2 +1,72 @@
-package com.productivity_helper.app.repo.user;public class AppUserRepositoryTest {
+package com.productivity_helper.app.repo.user;
+
+import com.productivity_helper.app.model.user.AppUser;
+import com.productivity_helper.app.model.user.AppUserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class AppUserRepositoryTest {
+
+    @Autowired
+    private AppUserRepository userRepo;
+    @Autowired
+    private AppUserRoleRepository userRoleRepo;
+
+    @BeforeEach
+    void setUp() {
+        AppUserRole admin = new AppUserRole("ADMIN");
+        AppUserRole user = new AppUserRole("USER");
+        AppUserRole guest = new AppUserRole("GUEST");
+        userRoleRepo.save(admin);
+        userRoleRepo.save(user);
+        userRoleRepo.save(guest);
+
+        AppUser john = new AppUser("John", "Doe", "john@mail.com", "password123");
+        AppUser mark = new AppUser("Mark", "Doe", "mark@mail.com", "password123");
+        AppUser ada = new AppUser("Ada", "Doe", "ada@mail.com", "password123");
+        AppUser userWithNoRoles = new AppUser("Ada", "Doe", "unknown@mail.com", "password123");
+        userRepo.save(john);
+        userRepo.save(mark);
+        userRepo.save(ada);
+        userRepo.save(userWithNoRoles);
+
+        john.addRoleToUser(admin);
+        john.addRoleToUser(user);
+        john.addRoleToUser(guest);
+
+        mark.addRoleToUser(user);
+        mark.addRoleToUser(guest);
+
+        ada.addRoleToUser(guest);
+    }
+
+    @Test
+    public void shouldFindAllUsers() {
+        List<AppUser> all = userRepo.findAll();
+
+        assertThat(all).isNotNull();
+        assertThat(all.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldFindUserByEmail() {
+        String mail = "john@mail.com";
+
+        Optional<AppUser> foundUser = userRepo.findByEmail(mail);
+
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getEmail()).isEqualTo(mail);
+    }
 }
--- src/test/java/com/productivity_helper/app/repo/user/AppUserRoleTest.java
+++ src/test/java/com/productivity_helper/app/repo/user/AppUserRoleTest.java
@@ -1,2 +1,51 @@
-package com.productivity_helper.app.repo.user;public class AppUserRoleTest {
+package com.productivity_helper.app.repo.user;
+
+import com.productivity_helper.app.model.user.AppUserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class AppUserRoleTest {
+
+    @Autowired
+    private AppUserRoleRepository userRoleRepo;
+
+    @BeforeEach
+    void setUp() {
+        AppUserRole admin = new AppUserRole("ADMIN");
+        AppUserRole user = new AppUserRole("USER");
+        AppUserRole guest = new AppUserRole("GUEST");
+
+        userRoleRepo.save(admin);
+        userRoleRepo.save(user);
+        userRoleRepo.save(guest);
+    }
+
+    @Test
+    public void shouldFindAllRoles() {
+        List<AppUserRole> all = userRoleRepo.findAll();
+
+        assertThat(all).isNotNull();
+        assertThat(all.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void shouldFindRoleByName() {
+        Optional<AppUserRole> admin = userRoleRepo.findByName("ADMIN");
+
+        assertThat(admin).isPresent();
+        assertThat(admin.get()).isNotNull();
+        assertThat(admin.get().getName()).isEqualTo("ADMIN");
+
+    }
 }
