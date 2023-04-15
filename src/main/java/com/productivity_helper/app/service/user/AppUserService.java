180eb958d7fed45bb1b40ff22f7f7ca4eecfaea6
Filip <filip7243@gmail.com>
2023-04-15 11:58:49 +0200

    creteUser() made

--- src/main/java/com/productivity_helper/app/service/user/AppUserService.java
+++ src/main/java/com/productivity_helper/app/service/user/AppUserService.java
@@ -1,6 +1,10 @@
 package com.productivity_helper.app.service.user;
 
+import com.productivity_helper.app.controller.user.RegisterRequest;
+import com.productivity_helper.app.model.user.AppUser;
 import com.productivity_helper.app.repo.user.AppUserRepository;
+import com.productivity_helper.app.repo.user.AppUserRoleRepository;
+import com.productivity_helper.app.security.CustomPasswordEncoder;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,10 +16,30 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository userRepo;
+    private final AppUserRoleRepository roleRepo;
+    private final CustomPasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepo.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+
+    public String createUser(RegisterRequest registerRequest) {
+        String username = registerRequest.username();
+        if (userRepo.existsByEmail(username)) {
+            throw new RuntimeException("User with email already exists!");
+        }
+
+        String encodedPassword = passwordEncoder.passwordEncoder().encode(registerRequest.password());
+        AppUser user = new AppUser(registerRequest.firstName(),
+                registerRequest.lastName(),
+                registerRequest.username(),
+                encodedPassword
+        );
+        // TODO: add basic role
+        userRepo.save(user);
+
+        return user.getUsername();
     }
 }
