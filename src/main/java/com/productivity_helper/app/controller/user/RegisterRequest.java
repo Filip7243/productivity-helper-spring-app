ba34aa323eb010ab7955d9866b10f4a8d6401a19
Filip <filip7243@gmail.com>
2023-04-15 11:55:15 +0200

    added login/register reuqests records

--- src/main/java/com/productivity_helper/app/controller/user/RegisterRequest.java
+++ src/main/java/com/productivity_helper/app/controller/user/RegisterRequest.java
@@ -1,2 +1,4 @@
-package com.productivity_helper.app.controller.user;public class RegisterRequest {
+package com.productivity_helper.app.controller.user;
+
+public record RegisterRequest(String firstName, String lastName, String username, String password) {
 }
