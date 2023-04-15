6a49fd3da04a81a712f943dd24166f67ebad8ab6
Filip <filip7243@gmail.com>
2023-04-15 11:59:15 +0200

    created auth service

--- src/main/java/com/productivity_helper/app/service/user/AuthService.java
+++ src/main/java/com/productivity_helper/app/service/user/AuthService.java
@@ -1,2 +1,50 @@
-package com.productivity_helper.app.service.user;public class AuthService {
+package com.productivity_helper.app.service.user;
+
+import com.productivity_helper.app.controller.user.LoginRequest;
+import com.productivity_helper.app.controller.user.RegisterRequest;
+import com.productivity_helper.app.security.jwt.JwtResponse;
+import com.productivity_helper.app.security.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authManager;
+    private final AppUserService userService;
+    private final JwtUtils jwtUtils;
+
+    public JwtResponse signIn(LoginRequest loginRequest) {
+        String username = loginRequest.username();
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(
+                username,
+                loginRequest.password()
+        ));
+
+        return getJwtResponse(username);
+    }
+
+    public JwtResponse signUp(RegisterRequest registerRequest) {
+        String username = userService.createUser(registerRequest);
+        return getJwtResponse(username);
+    }
+
+    private JwtResponse getJwtResponse(String username) {
+        UserDetails user = userService.loadUserByUsername(username);
+        String token = jwtUtils.generateToken(user);
+        String refreshToken = jwtUtils.generateRefreshToken(user);
+        List<String> authorities = user.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority).toList();
+
+        return new JwtResponse(
+                username, token, refreshToken, authorities
+        );
+    }
 }
