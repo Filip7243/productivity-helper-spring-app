6aa7f83506e584e1591d7ffccbc798197040a5f6
Filip <filip7243@gmail.com>
2023-04-19 13:57:58 +0200

    basic service methods

--- src/main/java/com/productivity_helper/app/service/todolist/ToDoItemService.java
+++ src/main/java/com/productivity_helper/app/service/todolist/ToDoItemService.java
@@ -1,2 +1,54 @@
-package com.productivity_helper.app.service.todolist;public class ToDoItemService {
+package com.productivity_helper.app.service.todolist;
+
+import com.productivity_helper.app.model.todolist.ToDoItem;
+import com.productivity_helper.app.model.todolist.ToDoItemDto;
+import com.productivity_helper.app.model.user.AppUser;
+import com.productivity_helper.app.repo.todolist.ToDoItemRepository;
+import com.productivity_helper.app.repo.user.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ToDoItemService {
+
+    private final ToDoItemRepository itemRepo;
+    private final AppUserRepository userRepo;
+
+    public Long addTask(ToDoItemDto task, Long userId) {
+        AppUser user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+
+        ToDoItem newTask = new ToDoItem(task.text(), task.dueDate(), user);
+        itemRepo.save(newTask);
+
+        return newTask.getId();
+    }
+
+    public void updateTask(ToDoItemDto updatedTask, Long taskId) {
+        ToDoItem task = itemRepo.findById(taskId)
+                .orElseThrow(() -> new RuntimeException("Task not found!"));
+
+        task.setText(updatedTask.text());
+        task.setDueDate(updatedTask.dueDate());
+
+        itemRepo.save(task);
+    }
+
+    public void markTaskAsDone(Long taskId) {
+        ToDoItem task = itemRepo.findById(taskId)
+                .orElseThrow(() -> new RuntimeException("Task not found!"));
+        task.setIsDone(true);
+
+        itemRepo.save(task);
+    }
+
+    public Long deleteTask(Long taskId) {
+        ToDoItem task = itemRepo.findById(taskId)
+                .orElseThrow(() -> new RuntimeException("Task not found!"));
+
+        itemRepo.deleteById(taskId);
+
+        return task.getId();
+    }
 }
