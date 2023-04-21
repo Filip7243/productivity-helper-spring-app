b19918431bdfe945ebe64303150940ef6dda7d4d
Filip <filip7243@gmail.com>
2023-04-21 13:33:16 +0200

    created tests

--- src/test/java/com/productivity_helper/app/service/todolist/ToDoItemServiceTests.java
+++ src/test/java/com/productivity_helper/app/service/todolist/ToDoItemServiceTests.java
@@ -1,5 +1,7 @@
 package com.productivity_helper.app.service.todolist;
 
+import com.productivity_helper.app.model.todolist.ToDoItem;
+import com.productivity_helper.app.model.todolist.ToDoItemDto;
 import com.productivity_helper.app.model.user.AppUser;
 import com.productivity_helper.app.repo.todolist.ToDoItemRepository;
 import com.productivity_helper.app.repo.user.AppUserRepository;
@@ -9,8 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,6 +35,50 @@ public class ToDoItemServiceTests {
 
     @Test
     public void canAddTask() {
-        when(userRepo.findById(any())).thenReturn(Optional.of(new AppUser()));
+        Long id = any();
+        Optional<AppUser> user = Optional.of(new AppUser());
+        ToDoItemDto item = new ToDoItemDto("test", new Date());
+
+        when(userRepo.findById(id)).thenReturn(user);
+
+        Long taskId = itemService.addTask(item, user.get().getId());
+        // TODO: repair it
+    }
+
+    @Test
+    public void canUpdateTask() {
+        Long id = any();
+        ToDoItemDto updatedTask = new ToDoItemDto("text", new Date());
+        Optional<ToDoItem> item = Optional.of(new ToDoItem());
+
+        when(itemRepo.findById(id)).thenReturn(item);
+
+        ToDoItemDto returnedItem = itemService.updateTask(updatedTask, item.get().getId());
+
+        assertThat(returnedItem).isNotNull();
+        assertThat(returnedItem.text()).isEqualTo(updatedTask.text());
+        assertThat(returnedItem.dueDate()).isEqualTo(updatedTask.dueDate());
+    }
+
+    @Test
+    public void canMarkTaskAsDone() {
+        Long id = any();
+
+        when(itemRepo.findById(id)).thenReturn(Optional.of(new ToDoItem()));
+
+        boolean isDone = itemService.markTaskAsDone(id);
+
+        assertThat(isDone).isTrue();
+    }
+
+    @Test
+    public void canDeleteTaskAsDone() {
+        Long id = any();
+
+        when(itemRepo.findById(id)).thenReturn(Optional.of(new ToDoItem()));
+
+        Long taskId = itemService.deleteTask(id);
+
+        // TODO: improve it
     }
 }
